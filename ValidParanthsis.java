5b676d816e33e71355668bc7883f2eb8593a5117
GouravSaini <mrgoravsainimrt@gmail.com>
2024-06-19 16:25:14 +0530

    validparanthsis

---
+++ ValidParanthsis.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ValidParanthsis{
+    public static boolean isvalid(String str){
+        char arr[] = str.toCharArray();
+        Stack<Character> map = new Stack<>();
+
+        for(char ele: arr){
+            if(ele == '(' || ele == '{' || ele =='['){
+               map.push(ele);
+            continue;
+            }else if(map.empty()){
+               return false;
+            }
+            char top = map.pop();
+        if(top == '(' && ele != ')'){
+            return false;
+        }else if(top =='{' && ele != '}'){
+            return false;
+        }else if(top == '[' && ele != ']'){
+            return false;
+        }
+
+        }
+        return(map.empty()==true);
+        
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string:");
+        String str = sc.nextLine();
+        System.out.println("is givan string valid "+isvalid(str));
+    }
+}
